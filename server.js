78443f7e47b380d80248397c935a26990334d73c
Giridhar Jayate <gireedharjayate@gmail.com>
2026-02-13 02:05:21 +0530

    Fixed PORT for Render

--- server.js
+++ server.js
@@ -116,6 +116,8 @@ app.get("/admitcard/:id", async (req, res) => {
     res.render("admitcard", { applicant });
 });
 
-app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
